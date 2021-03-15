2f54d7b0db75549aa05957baa5030615253b43eb
ich777 <ich777@noreply.example.org>
2021-03-15 11:06:47 +0100

    „source/makepkg.sh“ ändern

--- source/makepkg.sh
+++ source/makepkg.sh
@@ -7,7 +7,7 @@ VERSION="$(date +'%Y.%m.%d')"
 mkdir -p $TMP_DIR/$VERSION
 cd $TMP_DIR/$VERSION
 cp --parents -R $BASE_DIR/$PLUGIN_NAME/ $TMP_DIR/$VERSION/
-cp --parents -R /usr/bin/intel_gpu_top /usr/bin/intel_gpu_frequency $TMP_DIR/$VERSION
+cp --parents -R /usr/bin/intel_gpu_top $TMP_DIR/$VERSION
 chmod -R 755 $TMP_DIR/$VERSION
 rm $TMP_DIR/$VERSION/$BASE_DIR/$PLUGIN_NAME/README.md
 makepkg -l y -c y $TMP_DIR/$PLUGIN_NAME-$VERSION.txz
